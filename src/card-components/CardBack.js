46d362b91ca93ec78244378035b60e5c5584ac6a
Sabrina Jones <sabrina.jones87@gmail.com>
2021-06-01 06:01:36 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -21,11 +21,11 @@ export default class CardBack extends Component {
   render() {
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title">{this.props.title}</h3>
         <span />
         {this.generateRatingElement()}
         <span />
-        <h5 className="genres"></h5>
+       <h5 className="genres">{this.props.genres.join(', ')}</h5>
       </div>
     )
   }
